930ae0d21db8818e24beb1da156645f692547074
Paul Ostos <pauljostos@gmail.com>
2024-12-09 21:33:48 -0600

    Refactored the class by breaking it into modular methods for better readability and maintainability.

--- GarminFitnessTracker.java
+++ GarminFitnessTracker.java
@@ -6,70 +6,162 @@ import java.util.Map;
 
 public class GarminFitnessTracker {
 
+	// ***************************************************************
+	//
+	// Method: main()
+	//
+	// Description: The main method of the program
+	//
+	// Parameters: String array
+	//
+	// Returns: N/A
+	//
+	// **************************************************************
     public static void main(String[] args) {
-
-        GarminFitnessTracker obj = new GarminFitnessTracker(); 
-        obj.handleActivities();
-        
+        GarminFitnessTracker obj = new GarminFitnessTracker();
+        obj.handleActivities("Activities.csv");
     }
 
-    public void handleActivities(){
+    // ***************************************************************
+	//
+	// Method: 
+	//
+	// Description: 
+	//
+	// Parameters: 
+	//
+	// Returns: 
+	//
+	// **************************************************************
 
-        String csvFile = "Activities.csv"; // path to CSV file
+    private void handleActivities(String csvFile) {
+        Map<Integer, YearData> yearDataMap = parseCSV(csvFile);
+        YearlyStats(yearDataMap);
+    }
+
+    // ***************************************************************
+	//
+	// Method: 
+	//
+	// Description: 
+	//
+	// Parameters: 
+	//
+	// Returns: 
+	//
+	// **************************************************************
+
+    private Map<Integer, YearData> parseCSV(String csvFile) {
+        Map<Integer, YearData> yearDataMap = new HashMap<>();
+        String csvSplitBy = ",";
         String line;
-        String csvSplitBy = ","; // defines the delimiter
-
-        // create map to store total distance and activity count per year
-        Map<Integer, YearData> yearDataMap = new HashMap<>(); 
-        double bikeMilage = 0; // tracker for total miles ridden on Caad13 bicycle
-        System.out.println("----------------------------");
 
         try (BufferedReader br = new BufferedReader(new FileReader(csvFile))) {
-            // read the header line to skip over it (columns tab)
-            String header = br.readLine();
+            // skip the header line
+            br.readLine();
 
-            // loop through each line in the CSV
             while ((line = br.readLine()) != null) {
-                // Split each line by the comma delimiter
                 String[] data = line.split(csvSplitBy);
-
-                String date = data[1]; 
-                String activity = data[3]; 
-                double distance = Double.parseDouble(data[4].replace("\"","".trim())); 
-
-                // extract the year from the date
-                int year = Integer.parseInt(date.substring(0,4)); // date ex. 2020-11-13 17:41:02
-
-                // update the map with the distance and count
-                YearData yearData = yearDataMap.getOrDefault(year, new YearData());
-                yearData.addDistance(distance);
-                yearData.incrementActivityCount(); 
-
-                yearDataMap.put(year, yearData);
-
+                processActivityData(data, yearDataMap);
             }
-
-            // output the results for each year
-            for (Map.Entry<Integer, YearData> entry: yearDataMap.entrySet()){
-                int year = entry.getKey(); 
-                YearData yearData = entry.getValue(); 
-                double totalDistance = yearData.getTotalDistance(); 
-                int activityCount = yearData.getActivityCount(); 
-                double averageDistance = yearData.getAverageDistance(); 
-                bikeMilage += totalDistance; // track the total miles ridden on the Caad13 bike
-
-                System.out.println("Year: " + year);
-                System.out.printf("Total Distance: %.2f miles\n", totalDistance);
-                System.out.println("Average Ride Distance: " + String.format("%.2f", averageDistance) + " miles");
-                System.out.println("----------------------------");
-            }
-            System.out.println("\n-------------------------------------------");
-            System.out.println("Total miles ridden on Caad13: " + bikeMilage );
-            System.out.println("-------------------------------------------");
-
         } catch (IOException e) {
+            System.err.println("Error Parsing CSV file.");;
             e.printStackTrace();
         }
+        return yearDataMap;
+    }
 
+    // ***************************************************************
+	//
+	// Method: 
+	//
+	// Description: 
+	//
+	// Parameters: 
+	//
+	// Returns: 
+	//
+	// **************************************************************
+
+    private void processActivityData(String[] data, Map<Integer, YearData> yearDataMap) {
+        String date = data[1];
+        String activity = data[3];
+        double distance = Double.parseDouble(data[4].replace("\"", "").trim());
+
+        // Extract the year from the date
+        int year = Integer.parseInt(date.substring(0, 4));
+
+        // Update the map with the distance and activity count
+        YearData yearData = yearDataMap.getOrDefault(year, new YearData());
+        yearData.addDistance(distance);
+        yearData.incrementActivityCount();
+
+        yearDataMap.put(year, yearData);
+    }
+
+    // ***************************************************************
+	//
+	// Method: 
+	//
+	// Description: 
+	//
+	// Parameters: 
+	//
+	// Returns: 
+	//
+	// **************************************************************
+
+    private void YearlyStats(Map<Integer, YearData> yearDataMap) {
+        double bikeMilage = 0;
+
+        for (Map.Entry<Integer, YearData> entry : yearDataMap.entrySet()) {
+            int year = entry.getKey();
+            YearData yearData = entry.getValue();
+            double totalDistance = yearData.getTotalDistance();
+            int activityCount = yearData.getActivityCount();
+            double averageDistance = yearData.getAverageDistance();
+
+            bikeMilage += totalDistance;
+            displayYearlyStats(year, totalDistance, averageDistance);
+        }
+        displayTotalMiles(bikeMilage);
+    }
+
+    // ***************************************************************
+	//
+	// Method: 
+	//
+	// Description: 
+	//
+	// Parameters: 
+	//
+	// Returns: 
+	//
+	// **************************************************************
+
+    public void displayYearlyStats(int year, double totalDistance, double averageDistance){
+        System.out.println("----------------------------");
+        System.out.println("Year: " + year);
+        System.out.printf("Total Distance: %.2f miles\n", totalDistance);
+        System.out.println("Average Ride Distance: " + String.format("%.2f", averageDistance) + " miles");
+        System.out.println("----------------------------");
+    }
+
+    // ***************************************************************
+	//
+	// Method: 
+	//
+	// Description: 
+	//
+	// Parameters: 
+	//
+	// Returns: 
+	//
+	// **************************************************************
+
+    public void displayTotalMiles(double bikeMilage){
+        System.out.println("\n-------------------------------------------");
+        System.out.println("Total miles ridden on Caad13: " + bikeMilage);
+        System.out.println("-------------------------------------------");
     }
 }
